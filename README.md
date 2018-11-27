7211060f95eae31dbd22a833fc9fa919fddc4781
jiang.an <jarancn@gmail.com>
2018-11-27 17:52:52 +0800

    periodic task

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ pip install Celery
 celery -A tasks worker --loglevel=info
 ```
 
+以下在另一个终端
 ```
 python main.py
+```
+
+```
+celery -A tasks beat
 ```
--- tasks.py
+++ tasks.py
@@ -1,8 +1,25 @@
 from celery import Celery
 
-app = Celery('tasks', broker='amqp://XXXX:XXXX@XXXX:XXXX//')
+app = Celery()
+
+app.config_from_object('celeryconfig')
 
 @app.task
 def add(x, y):
+    print 'test '
     return x + y
+
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     # Calls test('hello') every 10 seconds.
+#     sender.add_periodic_task(2.0, add.s(1, 2))
+
+    # Calls test('world') every 30 seconds
+    # sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+    # Executes every Monday morning at 7:30 a.m.
+    # sender.add_periodic_task(
+    #     crontab(hour=7, minute=30, day_of_week=1),
+    #     test.s('Happy Mondays!'),
+    # )
 
